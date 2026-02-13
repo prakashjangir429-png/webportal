04c6e2417ed2c46bcd837deb15844a421bd8f0ec
PrakashGatway <abroadgateway370@gmail.com>
2026-02-13 16:50:54 +0530

    dd

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3030/api/v1', // Replace with your API base URL
+  baseURL: 'https://payservices.online/api/v1', // Replace with your API base URL
   // baseURL:'https://mypanel-cmnj.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',

9b752d781bad8e7f8b6df9e29c8859065d1ac128
prakashjangir429-png <prakashjangir429@gmail.com>
2026-03-16 22:02:56 +0530

    dd

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.mandlal.shop/api/v1', // Replace with your API base URL
+  baseURL: 'https://payments-le28.onrender.com/api/v1', // Replace with your API base URL
   // baseURL: 'http://localhost:3030/api/v1', // Replace with your API base URL
   // baseURL:'https://mypanel-cmnj.onrender.com/api/v1',
   headers: {
